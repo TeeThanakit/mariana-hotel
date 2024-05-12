d3cc43ded0d615de95268df9235d3fa6aebdb16b
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-05-12 22:42:26 +0700

    feat: add navbar to app layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import MainNavigator from "@/components/main-navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <MainNavigator/>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
