bbe715f246e16dbda64252f7bc471b56b2c41ddf
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-05-17 20:59:02 +0700

    ?

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,1 +1,12 @@
-export { auth as middleware } from "@/auth"
+export { auth as middleware } from "@/auth";
+
+// export default auth((req) => {
+//   if (!req.auth) {
+//     const url = req.url.replace(req.nextUrl.pathname, "/login");
+//     return Response.redirect(url);
+//   }
+// });
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
