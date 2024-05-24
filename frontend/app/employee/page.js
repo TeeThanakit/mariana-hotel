c7374eb9faa9288cdd760a17e753c2a033836a92
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-05-24 12:33:24 +0700

    test: check session

--- frontend/app/employee/page.js
+++ frontend/app/employee/page.js
@@ -1,10 +1,16 @@
 import { auth } from "@/auth";
 
-
-export default async function StaffPage(){
-    const session = await auth();
-    return <div>
-        <h1>Employee area</h1>
-        <p>{session?.user?.email}</p>
+export default async function StaffPage() {
+  const session = await auth();
+  if(session){
+    console.log("Print session", session);
+  }
+  return (
+    <div>
+      <h1>Employee area</h1>
+      <p>{session?.user?.email}</p>
+      <p>{session?.user?.username}</p>
+      <p>{session?.user?.role}</p>
     </div>
+  );
 }
