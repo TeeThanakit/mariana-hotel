74603b48aa2491e0f95ab67d3a9676588f03530c
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-05-25 21:26:54 +0700

    fix payment path

--- frontend/components/staff-role-nav.js
+++ frontend/components/staff-role-nav.js
@@ -5,8 +5,7 @@ export function RoleStaffNav() {
   return (
     <div>
         <div className="flex justify-center items-center gap-3 text-red-500">
-            <Link href="/employee/staff">Payment</Link>
-            <Link href="/employee/staff">Room</Link>
+            <Link href="/employee/staff/payment">Payment</Link>
             <Link href="/employee/staff/contactus">Contact</Link>
         </div>
     </div>
