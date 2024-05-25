3aed745e7f535f2cc861c5a506c05158eedfe532
CirrusPreeminent <153276550+CirrusPreeminent@users.noreply.github.com>
2024-05-26 00:19:21 +0700

    fix img

--- frontend/app/about/page.js
+++ frontend/app/about/page.js
@@ -64,7 +64,7 @@ export default function About() {
               roomType="deluxe"
             />
             <RoomCard
-              image="/images/studio.jpg"
+              image="/images/studios.jpg"
               title="Studio"
               description="Our Master size provides the ultimate in luxury and comfort."
               roomType="master-suite"
