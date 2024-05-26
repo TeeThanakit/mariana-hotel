5d7064b31eb854e7de1d59df3cf200e660e19c9f
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-05-26 19:00:37 +0700

    change message

--- frontend/app/employee/page.js
+++ frontend/app/employee/page.js
@@ -9,7 +9,7 @@ export default async function StaffPage() {
   return (
     <div className="text-center text-3xl mt-5">
       {!session ? (
-        <h1>Here is a employee area, you gotta login first </h1>
+        <h1>Here is a employee area, you need to login first </h1>
       ) : (
         <h1>You are logged in</h1>
       )}
